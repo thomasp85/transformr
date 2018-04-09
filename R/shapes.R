e2af138d7eba29e2abcf7c9ba2923205239b78c1
Thomas Lin Pedersen <thomasp85@gmail.com>
2018-04-09 16:22:52 +0200

    document shapes

--- R/shapes.R
+++ R/shapes.R
@@ -1,20 +1,52 @@
+#' Some different geometries to play with
+#'
+#' These functions are provided to allow you to play with somee simple shapes as
+#' you explore `transformr` and are also used in the examples for the different
+#' tween functions. All geometries can be returned as either a standard
+#' `data.frame` with `x`, `y`, and `id` column, or as an sf geometry of the
+#' appropriate type.
+#'
+#' @param st Logical. Should the geometry be returned as an `sf` feature?
+#' @param detail The number of points defining the shape
+#' @param n For `poly_circles` the number of circles, for `poly_star` and
+#' `poly_star_hole` the number of 'arms', and for `point_random` the number of
+#' points
+#' @param r,r1 The radius of the geometry. `r` gives the radius of the circles
+#' in `poly_circles` and `r1` gives the inner radius for
+#' `poly_star`/`poly_star_hole`, thus determining how pointy it is
+#' @param windings The number of revolutions in the spiral
+#' @param w1,w2 The frequency for the two sine waves
+#' @param dim the number of rows and columns in the grid
+#'
+#' @return Either a data.frame or an sf feature depending on the value of `st`
+#'
+#' @name simple_shapes
+#' @rdname simple_shapes
+#'
+#' @examples
+#' # Create a 7-pointed star
+#' poly_star(n = 7)
+#'
+
+#' @rdname simple_shapes
 #' @export
-poly_circle <- function(st = FALSE, n = 360) {
-  i <- seq(0, 2*pi, length.out = n+1)[-n-1]
+poly_circle <- function(st = FALSE, detail = 360) {
+  i <- seq(0, 2*pi, length.out = detail+1)[-detail-1]
   x <- sin(i)
   y = cos(i)
   if (st) {
-    st_polygon(list(cbind(x, y)[c(seq_len(n), 1), , drop = FALSE]))
+    st_polygon(list(cbind(x, y)[c(seq_len(detail), 1), , drop = FALSE]))
   } else {
     data.frame(x = x, y = y, id = 1L)
   }
 }
+#' @rdname simple_shapes
 #' @export
-poly_circles <- function(st = FALSE, n = 3, r = 0.25) {
+poly_circles <- function(st = FALSE, n = 3, r = 0.25, detail = 360) {
   i <- seq(0, 2*pi, length.out = n+1)[-n-1]
   x <- sin(i)
   y = cos(i)
-  d <- poly_circle()
+  d <- poly_circle(detail = detail)
   d_small <- d
   d_small$x <- d_small$x * r
   d_small$y <- d_small$y * r
@@ -39,6 +71,7 @@ poly_circles <- function(st = FALSE, n = 3, r = 0.25) {
     rbind(d1,d2,d3)
   }
 }
+#' @rdname simple_shapes
 #' @export
 poly_star <- function(st = FALSE, n = 5, r1 = 0.5) {
   d <- poly_circle(n = n*2)
@@ -49,6 +82,7 @@ poly_star <- function(st = FALSE, n = 5, r1 = 0.5) {
   }
   d
 }
+#' @rdname simple_shapes
 #' @export
 poly_star_hole <- function(st = FALSE, n = 5, r1 = 0.5) {
   d <- poly_star(n = n, r1 = r1)
@@ -68,6 +102,7 @@ poly_star_hole <- function(st = FALSE, n = 5, r1 = 0.5) {
     )
   }
 }
+#' @rdname simple_shapes
 #' @export
 path_spiral <- function(st = FALSE, windings = 5) {
   n = 50 * windings
@@ -81,6 +116,7 @@ path_spiral <- function(st = FALSE, windings = 5) {
     data.frame(x = x, y = y, id = 1L)
   }
 }
+#' @rdname simple_shapes
 #' @export
 path_waves <- function(st = FALSE, w1 = 7, w2 = 11) {
   x <- seq(-1, 1, length.out = 150)
@@ -95,8 +131,10 @@ path_waves <- function(st = FALSE, w1 = 7, w2 = 11) {
     data.frame(x = rep(x, 2), y = c(y1, y2), id = rep(c(1L, 2L), each = length(x)))
   }
 }
+#' @rdname simple_shapes
 #' @export
 #' @importFrom sf st_multipoint
+#' @importFrom stats runif
 point_random <- function(st = FALSE, n = 10) {
   x <- runif(10, min = -1, max = 1)
   y <- runif(10, min = -1, max = 1)
@@ -106,6 +144,7 @@ point_random <- function(st = FALSE, n = 10) {
     data.frame(x = x, y = y, i = seq_len(n))
   }
 }
+#' @rdname simple_shapes
 #' @export
 #' @importFrom sf st_multipoint
 point_grid <- function(st = FALSE, dim = 5) {
